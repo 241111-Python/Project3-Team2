90b2376c733d32dba5ae7454abd5b5bada346f62
Romeo-Ashkar <romeoashkar01@gmail.com>
2024-12-05 13:53:55 -0800

    Normalized death cause correlation

--- death_causes.py
+++ death_causes.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 deaths_df = pd.read_csv("maybeData/cause_of_deaths.csv")
+population_df = pd.read_csv("maybeData/world_population.csv")
+
 deaths_df.columns = deaths_df.columns.str.strip()
 deaths_df.columns = deaths_df.columns.str.replace(' ', '_')
 deaths_df.columns = deaths_df.columns.str.replace('/', '_')
@@ -12,9 +14,18 @@ df = deaths_df.drop(columns=['Code'])
 df.rename(columns={'Country_Territory': 'Country'}, inplace=True)
 grouped_deaths_df = df.groupby('Country').mean().reset_index()
 
-death_causes_df = grouped_deaths_df.drop(columns=['Country', 'Year', 'Conflict_and_Terrorism', 'Malaria', 'HIV_AIDS', 'Exposure_to_Forces_of_Nature'])
+population_df.rename(columns={'Country/Territory': 'Country'}, inplace=True)
+pop_df = population_df[['Country','2020 Population', '2010 Population', '2000 Population', '1990 Population']]
+pop_df['Avg_Population'] = pop_df[['2020 Population', '2010 Population', '2000 Population', '1990 Population']].mean(axis=1)
 
-print(death_causes_df)
+merged_df = pd.merge(grouped_deaths_df, pop_df[['Country', 'Avg_Population']], on='Country', how='left')
+
+death_causes_df = merged_df.drop(columns=['Country', 'Year'])
+
+for column in death_causes_df.columns:
+    death_causes_df[column] = (death_causes_df[column] / merged_df['Avg_Population']) * 100000
+
+death_causes_df = death_causes_df.drop(columns=['Avg_Population'])
 
 correlation_matrix = death_causes_df.corr()
 plt.figure(figsize=(18, 16))
@@ -22,4 +33,4 @@ sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="coolwarm", vmin=-1,
 plt.xticks(rotation=90, ha='right')
 plt.yticks(rotation=0, va='center')
 plt.tight_layout()
-plt.show()
+plt.savefig("Death_correlation.png")
