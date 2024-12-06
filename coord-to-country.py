56422ef672f172189828ba3d36816a90a7010b82
TreyBlack01 <blacktrey000@gmail.com>
2024-12-06 14:41:53 -0500

    Placed the code to convert coordinates to countries in its own file. Used a bit of Romeo's code to normalize the death causes and created a heatmap to compare against the meteorites

---
+++ coord-to-country.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from geopy.geocoders import Nominatim
+import ast
+geoloc = Nominatim(user_agent='meteorite-causes.py')
+def findCountry(coor):
+    try:
+        loc = geoloc.reverse(coor, exactly_one=True, language='en', timeout=10)
+        ad = loc.raw['address']
+        (ad.get('country',''))
+        return ad.get('country','')
+    except:
+        return 'Ocean'
+
+meteors = pd.read_csv(r"maybeData\Meteorite_Landings.csv", usecols=['year', 'GeoLocation'])
+meteors.dropna(inplace=True)
+meteors = meteors[meteors['GeoLocation']!="(0.0, 0.0)"]
+meteors = meteors[meteors['year']>=1990]
+meteors['GeoLocation'] = meteors['GeoLocation'].apply(ast.literal_eval)
+meteors['GeoLocation'] = meteors['GeoLocation'].apply(findCountry)
+meteors.to_csv(r"maybeData\Meteorite_Countries.csv")
